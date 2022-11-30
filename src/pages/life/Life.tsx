9d194a0e22ffb0501ee1d2dcc4c14ee8e073e37c
Upamanyu Ghose <upamanyu@Upamanyus-MBP.cust.communityfibre.co.uk>
2022-11-30 12:08:14 +0000

    Fixed router issues with Life page

--- src/pages/life/Life.tsx
+++ src/pages/life/Life.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 
-import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Image, Text} from '@chakra-ui/react';
+import { Github, Linkedin, FileText, Book, Mail } from 'lucide-react';
 
 import { useData } from 'common/data/Data';
+
 
 export const Life: FC = () => {
     const { data } = useData();
@@ -15,6 +17,73 @@ export const Life: FC = () => {
             h="100%"
             flexDirection={{ base: 'column', md: 'row' }}
         >
+            <Flex justifyContent="center" flex='0.3'>
+                <Image
+                    w={{ base: '200px', md: '400px' }}
+                    h={{ base: '200px', md: '400px' }}
+                    objectFit="cover"
+                    src={data.picture}
+                    borderRadius={{ base: '50%', md: 'xl' }}
+                />
+            </Flex>
+            <Box flex='0.7'>
+                <Heading fontSize="4xl" textAlign={{ base: 'center', md: 'left' }}>
+                    {data.name}
+                </Heading>
+                <Text fontWeight="600" textAlign={{ base: 'center', md: 'left' }} color="gray" pt='1'>
+                    {data.title}
+                </Text>
+                <Flex mt="8" gap="4" flexDirection={{ base: 'column', md: 'row' }}>
+                    <Button
+                        variant="outline"
+                        leftIcon={<Github width="8pt" />}
+                        onClick={() => window.open(data.github)}
+                        borderWidth="2px"
+                        size={{ base: 'lg', md: 'md' }}
+                    >
+                        GitHub
+                    </Button>
+                    <Button
+                        variant="outline"
+                        leftIcon={<Linkedin width="8pt" />}
+                        onClick={() => window.open(data.linkedin)}
+                        borderWidth="2px"
+                        size={{ base: 'lg', md: 'md' }}
+                    >
+                        LinkedIn
+                    </Button>
+                    <Button
+                        variant="outline"
+                        leftIcon={<Book width="8pt" />}
+                        onClick={() => window.open(data.google_scholar)}
+                        borderWidth="2px"
+                        size={{ base: 'lg', md: 'md' }}
+                    >
+                        G-Scholar
+                    </Button>
+                    <Button
+                        variant="outline"
+                        leftIcon={<Mail width="8pt" />}
+                        onClick={() => window.open(data.email)}
+                        borderWidth="2px"
+                        size={{ base: 'lg', md: 'md' }}
+                    >
+                        Mail
+                    </Button>
+                    <Button
+                        leftIcon={<FileText width="8pt" />}
+                        onClick={() => window.open(data.cv)}
+                        borderWidth="2px"
+                        borderColor="brand"
+                        size={{ base: 'lg', md: 'md' }}
+                    >
+                        CV
+                    </Button>
+                </Flex>
+                <Text mt="4" align="justify" fontWeight="500">
+                    {data.about}
+                </Text>
+            </Box>
         </Flex>
     );
 };
