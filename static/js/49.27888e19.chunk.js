e3a2366ca37858bef0180e7e4533aa13d12b01a8
titoghose <titoghose@gmail.com>
2022-11-30 12:19:12 +0000

    Deploying to gh-pages from @ titoghose/titoghose.github.io@c3d12972f5f505501f4de9e474fd45296c5485aa 🚀

---
+++ static/js/49.27888e19.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktito_portfolio=self.webpackChunktito_portfolio||[]).push([[49],{3049:function(t,e,i){i.r(e),i.d(e,{Life:function(){return u}});var n=i(5580),o=i(9797),r=i(184),u=function(){var t=(0,o.e)().data;return(0,r.jsx)(n.kC,{my:{base:0,md:4},w:"100%",gap:{base:4,md:8},h:"100%",flexDirection:{base:"column",md:"row"},children:(0,r.jsx)(n.xu,{flex:"0.7",children:(0,r.jsx)(n.xv,{mt:"4",align:"justify",fontWeight:"500",children:t.about})})})}}}]);
+//# sourceMappingURL=49.27888e19.chunk.js.map
